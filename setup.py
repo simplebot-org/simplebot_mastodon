4be22a4dd19fcbddca6b4ea2d35f2ff7bae66737
adbenitez <asieldbenitez@gmail.com>
2021-11-13 16:01:43 -0500

    improve setup.py

--- setup.py
+++ setup.py
@@ -1,32 +1,30 @@
 """Setup module installation."""
 
 import os
-import re
 
 from setuptools import find_packages, setup
 
 if __name__ == "__main__":
     MODULE_NAME = "simplebot_mastodon"
-    DESC = "A plugin for SimpleBot, a Delta Chat(http://delta.chat/) bot"
-
-    init_file = os.path.join(MODULE_NAME, "__init__.py")
-    with open(init_file) as fh:
-        version = re.search(r"__version__ = \'(.*?)\'", fh.read(), re.M).group(1)
+    DESC = "Mastodon/DeltaChat bridge."
+    KEYWORDS = "simplebot plugin deltachat mastodon bridge"
 
     with open("README.rst") as fh:
         long_description = fh.read()
-    with open("CHANGELOG.rst") as fh:
-        long_description += fh.read()
-    with open("LICENSE") as fh:
-        long_description += fh.read()
 
     setup(
         name=MODULE_NAME,
-        version=version,
+        setup_requires=["setuptools_scm"],
+        use_scm_version={
+            "root": ".",
+            "relative_to": __file__,
+            "tag_regex": r"^(?P<prefix>v)?(?P<version>[^\+]+)(?P<suffix>.*)?$",
+            "git_describe_command": "git describe --dirty --tags --long --match v*.*.*",
+        },
         description=DESC,
         long_description=long_description,
         long_description_content_type="text/x-rst",
-        keywords="simplebot plugin deltachat",
+        keywords=KEYWORDS,
         license="MPL",
         classifiers=[
             "Development Status :: 3 - Alpha",
@@ -48,6 +46,6 @@ if __name__ == "__main__":
             "pydub",
         ],
         entry_points={
-            "simplebot.plugins": "{0} = {0}".format(MODULE_NAME),
+            "simplebot.plugins": f"{MODULE_NAME} = {MODULE_NAME}",
         },
     )
